e239a4df266a99afab0e23537e75d5c878623529
Preetam D'Souza <preetamjdsouza@gmail.com>
2015-08-04 12:29:44 -0700

    Android.mk: clean up

--- Android.mk
+++ Android.mk
@@ -6,19 +6,14 @@ LOCAL_SRC_FILES := mlib.c
 include $(BUILD_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
-
 LOCAL_MODULE := mflinger
-
 LOCAL_SRC_FILES := mflinger.cpp
-
 LOCAL_CFLAGS := -DLOG_TAG=\"mflinger\"
-
 LOCAL_SHARED_LIBRARIES := \
     liblog \
     libutils \
     libgui \
     libmaru \
-
 include $(BUILD_EXECUTABLE)
 
 include $(CLEAR_VARS)
