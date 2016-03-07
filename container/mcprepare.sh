f9e89778418325236d3e2d8411caf81b7506d255
Preetam D'Souza <preetamjdsouza@gmail.com>
2016-03-06 18:05:26 -0800

    Sync container files from /system instead of /data
    This gets mcprepare ready to handle all container files being
    shipped on system.img in order to avoid needing to flash
    the userdata partition.
    
    Also create the /data/maru directory in init in order to make
    sure the SELinux file_context is set correctly.
    
    (This is missing the change to device-maru.mk that moves all
    the files from /data to /system because I was dumb and deleted
    my old local repo and added new stuff on top)

--- container/mcprepare.sh
+++ container/mcprepare.sh
@@ -1,9 +1,21 @@
 #!/system/bin/sh
 
-DST="/data/maru/containers/jessie"
-ROOTFS="$DST/jessie-rootfs.tar.gz"
+MARU_SYSTEM_DIR="/system/maru"
+MARU_DATA_DIR="/data/maru"
 
-if [ -d "$DST/rootfs" ] ; then
+SRC_DIR="$MARU_SYSTEM_DIR/containers/jessie"
+DST_DIR="$MARU_DATA_DIR/containers/jessie"
+ROOTFS="$SRC_DIR/jessie-rootfs.tar.gz"
+
+if [ ! -d "$DST_DIR" ] ; then
+    echo "copying maru files to /data..."
+    busybox cp -r "$MARU_SYSTEM_DIR" "/data/"
+
+    # not needed since we read rootfs from /system
+    busybox rm "$DST_DIR/jessie-rootfs.tar.gz"
+fi
+
+if [ -d "$DST_DIR/rootfs" ] ; then
     echo "rootfs already exists, nothing to be done" >&2
     exit 0
 fi
@@ -13,9 +25,8 @@ if [ ! -f $ROOTFS ] ; then
     exit 1
 fi
 
-echo "extracting $ROOTFS to $DST..."
-
-busybox tar xzf "$ROOTFS" -C "$DST"
+echo "extracting $ROOTFS to $DST_DIR..."
+busybox tar xzf "$ROOTFS" -C "$DST_DIR"
 
 if [ $? -ne 0 ] ; then
     echo "failed to extract rootfs" >&2
