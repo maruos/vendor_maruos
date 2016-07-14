c142fb577691208ba5e211c104f649e9a835ff47
Preetam D'Souza <preetamjdsouza@gmail.com>
2016-07-13 20:16:04 -0400

    use external/lxc headers

--- perspective/Android.mk
+++ perspective/Android.mk
@@ -6,7 +6,9 @@ LOCAL_MODULE := perspectived
 
 LOCAL_SRC_FILES := PerspectiveService.cpp
 
-LOCAL_C_INCLUDES += $(LOCAL_PATH)/../include
+LOCAL_C_INCLUDES += \
+	$(LOCAL_PATH)/../include \
+	external/lxc/src
 
 LOCAL_CFLAGS := -DLOG_TAG=\"perspectived\"
 
