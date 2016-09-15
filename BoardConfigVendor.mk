0ac5928213e3e894e5a67cb7879e8bc2241291cc
Preetam D'Souza <preetamjdsouza@gmail.com>
2016-09-15 15:44:48 -0400

    sepolicy: remove BOARD_SEPOLICY_UNION
    This was deprecated in M. All files found in BOARD_SEPOLICY_DIRS are
    automatically unioned now.
    
    Signed-off-by: Preetam D'Souza <preetamjdsouza@gmail.com>

--- BoardConfigVendor.mk
+++ BoardConfigVendor.mk
@@ -16,13 +16,3 @@
 #
 BOARD_SEPOLICY_DIRS += \
        vendor/maruos/sepolicy
-
-BOARD_SEPOLICY_UNION += \
-       service.te \
-       surfaceflinger.te \
-       perspectived.te \
-       mflinger.te \
-       mcprepare.te \
-       maru_files.te \
-       file_contexts \
-       service_contexts
